5642688ba94099719233212815387bf086ae6e48
nadinantire <nadinantire@gmail.com>
2019-12-05 11:24:36 +0000

    fix error

--- app/models/property.rb
+++ app/models/property.rb
@@ -3,7 +3,7 @@ class Property < ApplicationRecord
     validates :property, :rent, :adress,
         :building_age, presence: true
     validates :rent, numericality: true   
-    accepts_nested_attributes_for :stations, reject_if: :all_blank, :allow_destroy => true
+    accepts_nested_attributes_for :stations, :allow_destroy => true
         def check
             if how_many_minutes_walks.present? && how_many_minutes_walks1.present?
                 if how_many_minutes_walks <= how_many_minutes_walks1
